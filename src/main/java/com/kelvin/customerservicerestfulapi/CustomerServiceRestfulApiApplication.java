d5148b5550471c0e8352e31829511c561afe0b5d
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-01-22 14:10:15 +0100

    Project setup

---
+++ src/main/java/com/kelvin/customerservicerestfulapi/CustomerServiceRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.customerservicerestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceRestfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kelvin/customerservicerestfulapi/CustomerServiceRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.customerservicerestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
