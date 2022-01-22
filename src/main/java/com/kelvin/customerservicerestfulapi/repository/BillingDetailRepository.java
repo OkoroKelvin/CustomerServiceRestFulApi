c33c9d3035a459861788b9f5a15ebe762fedd233
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-01-22 14:58:46 +0100

    Implemented Repository Interface

--- src/main/java/com/kelvin/customerservicerestfulapi/repository/BillingDetailRepository.java
+++ src/main/java/com/kelvin/customerservicerestfulapi/repository/BillingDetailRepository.java
@@ -1,4 +1,15 @@
 package com.kelvin.customerservicerestfulapi.repository;
 
-public interface BillingDetailRepository {
+import com.kelvin.customerservicerestfulapi.model.BillingDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BillingDetailRepository extends JpaRepository<BillingDetail, Long> {
+
+    @Query("select b from BillingDetail b where b.customer.id = ?1")
+    List<BillingDetail> findAllByCustomerId(Long customerId);
 }
--- src/main/java/com/kelvin/customerservicerestfulapi/repository/CustomerRepository.java
+++ src/main/java/com/kelvin/customerservicerestfulapi/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.kelvin.customerservicerestfulapi.repository;
 
-public interface CustomerRepository {
+
+import com.kelvin.customerservicerestfulapi.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
